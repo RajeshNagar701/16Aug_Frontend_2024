af934697a102df68d83ef0a9751ed596a753a258
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-03-26 16:55:34 +0530

    first commit

---
+++ React/reacttask/src/App.js
@@ -0,0 +1,34 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Navbar from "./Navbar";
+
+
+/*
+Download & use Extension in React 
+
+es7 + react
+JavaScript and React/Redux snippets in ES7+ with Babel plugin features for VS Code
+Short code for program 
+imr    : import react
+imrc : import react with component
+
+rfce : create function component
+rce : create class component
+rcc :  create class component with export
+rcep  : create class component with proptype
+
+*/
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
--- React/reacttask/src/index.js
+++ React/reacttask/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Myapp from './Myapp';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
-   <h1>Hi i am index.js</h1>
-   <Myapp/>
+    <App />
   </React.StrictMode>
 );
