fc80322c3c4a7f14be703414e4dc8b0b4371b57b
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-04-04 16:58:15 +0530

    first

--- React/reacttask/src/App.js
+++ React/reacttask/src/App.js
@@ -1,7 +1,9 @@
 import Class_component from "./Component/Class_component";
 import Func_component from "./Component/Func_component";
+import React_css from "./CSS/React_css";
 import Main_Jsx from "./Jsx/Main_Jsx";
 import Main_layout from "./Layout/Main_layout";
+import Module_css from "./Module_css/Module_css";
 import Class_props from "./props/Class/Class_props";
 import Func_props from "./props/Func/Func_props";
 import Home from "./Website/pages/Home";
@@ -42,7 +44,12 @@ function App() {
 
         //Props
         //<Func_props/>
-        <Class_props/>
+        //<Class_props/>
+
+        // css
+        //<React_css/>
+        <Module_css/>
+
       }
      
     </div>
