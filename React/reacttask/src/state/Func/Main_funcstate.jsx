beec0c17f28209914d1371b85112215962e8c6c7
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-04-11 16:37:22 +0530

    first

--- React/reacttask/src/state/Func/Main_funcstate.jsx
+++ React/reacttask/src/state/Func/Main_funcstate.jsx
@@ -24,14 +24,58 @@ To use the useState Hook, we first need to import it into our component.
 */
 
 
-
-
-
-import React from 'react'
+import React, { useState } from 'react'
+import Func_img from './Func_img';
 
 function Main_funcstate() {
+
+  const [data, setData] = useState({
+    name: "Rajesh Nagar",
+    number: 1,
+    isImage: true
+  });
+
+  const [email,setEmail]=useState("raj@gmail.com");
+
+
+  const plus = () => {
+    setData({ ...data, number: data.number + 1 });
+  }
+  const minus = () => {
+    setData({ ...data, number: data.number - 1 });
+  }
   return (
     <div>
+      <button onClick={() => setData({ ...data, name: "Akash Nagar" })}>Change</button>
+      <h1>Hi my name is : {data.name}</h1>
+
+      <button onClick={() => plus()}>+</button>
+      <h1>{data.number}</h1>
+      <button onClick={() => minus()}>-</button>
+
+      <br />
+      <hr />
+      <br />
+
+      <button onClick={() => setData({...data, isImage: false })}>Hide</button>
+      <button onClick={() => setData({...data, isImage: true })}>Show</button>
+      <button onClick={() =>setData({...data, isImage: !data.isImage })}>Hide/Show</button>
+
+      {
+        data.isImage ? <Func_img /> : null
+      }
+
+      <br />
+      <hr />
+      <br />
+
+      <button onClick={()=>setEmail("raj@yahho.com")}>Change</button>
+      <h1>My email is :{email}</h1>
+
+
+      <br />
+      <hr />
+      <br />
 
     </div>
   )
